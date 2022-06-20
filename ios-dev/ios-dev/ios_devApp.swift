02e946c911ce79d7ccb3903c1eb39a69bac2428a
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-06-20 23:57:33 +0900

    Chore: build swiftUI

---
+++ ios-dev/ios-dev/ios_devApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_devApp.swift
+//  ios-dev
+//
+//  Created by apple developer academy on 2022/06/20.
+//
+
+import SwiftUI
+
+@main
+struct ios_devApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
