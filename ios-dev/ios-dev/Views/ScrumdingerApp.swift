30658bf21a5816cd0aefd212f05d55d61b9e9d31
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-06-26 22:44:21 +0900

    SwiftUI: Creating a Navigation Hierarchy

---
+++ ios-dev/ios-dev/Views/ScrumdingerApp.swift
@@ -0,0 +1,20 @@
+//
+//  ScrumdingerApp.swift
+//  ios-dev
+//
+//  Created by apple developer academy on 2022/06/26.
+//
+
+import SwiftUI
+
+struct ScrumdingerApp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScrumdingerApp_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumdingerApp()
+    }
+}
