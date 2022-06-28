00e7b085c54abcdcc9b1f82df287e04b3278693a
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-06-28 23:35:12 +0900

    SwiftUI: Section1 Add a Theme View

---
+++ ios-dev/ios-dev/Views/ThemeView.swift
@@ -0,0 +1,29 @@
+//
+//  ThemeView.swift
+//  ios-dev
+//
+//  Created by apple developer academy on 2022/06/28.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 4)
+                .fill(theme.mainColor)
+            Label(theme.name, systemImage: "paintpalette")
+                .padding(4)
+        }
+        .foregroundColor(theme.accentColor)
+       .fixedSize(horizontal: false, vertical: true)
+    }
+}
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView(theme: .buttercup)
+    }
+}
