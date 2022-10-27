bbe4c100bbd6cefdf1934d94412f3f5840ae388b
Blackraven93 <reblackraven@gmail.com>
2022-10-27 23:55:29 +0900

    Tutorial: Driving changes in your UI with state and bindings

---
+++ ios-dev/SwiftUIBoilerplate/Views/Layouts/Recipe/ContentListView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentListView.swift
+//  ios-dev
+//
+//  Created by blackRaven on 2022/10/27.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct ContentListView: View {
+    @Binding var selection: Recipe.ID?
+    let selectedSidebarItem: SidebarItem
+    @EnvironmentObject private var recipeBox: RecipeBox
+    @State private var recipeEditorConfig = RecipeEditorConfig()
+
+    var body: some View {
+        RecipeListView(selection: $selection, selectedSidebarItem: selectedSidebarItem)
+            .navigationTitle(selectedSidebarItem.title)
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        recipeEditorConfig.presentAddRecipe(sidebarItem: selectedSidebarItem)
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    .sheet(isPresented: $recipeEditorConfig.isPresented,
+                           onDismiss: didDismissEditor) {
+                        RecipeEditor(config: $recipeEditorConfig)
+                    }
+                }
+            }
+    }
+    
+    private func didDismissEditor() {
+        if recipeEditorConfig.shouldSaveChanges {
+            if recipeEditorConfig.recipe.isNew {
+                selection = recipeBox.add(recipeEditorConfig.recipe)
+            } else {
+                recipeBox.update(recipeEditorConfig.recipe)
+            }
+        }
+    }
+}
+
+struct ContentListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentListView()
+    }
+}
